cd72a17c35acddd90d2a19c39d8f7b0ee5f7be4a
MD-dev-007 <deebakbalaji07@gmail.com>
2025-04-17 23:02:16 +0530

    Fix CORS to allow GitHub Page

--- api/server.js
+++ api/server.js
@@ -11,19 +11,31 @@ connectDB();
 
 const app = express();
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// Configure CORS
+// Configure CORS with specific options
 app.use(cors({
-    origin: '*', // Allow all origins
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    credentials: false,
+    maxAge: 600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 }));
-
-// Add CORS headers manually for preflight requests
-app.options('*', cors());
 
 app.get("/", (req, res) => {
     res.json("hello world");
